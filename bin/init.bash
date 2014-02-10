9a0168d430ad7ef864af702e69f11c598bcc1fe1
Jesse Zhang <sbjesse@gmail.com>
2014-02-10 00:07:39 +0000

    bootstrap clones repo; Git caches credentials

--- bin/init.bash
+++ bin/init.bash
@@ -8,6 +8,16 @@ if !(type git && type make); then
 	sudo aptitude install -y git make
 fi
 git config --global color.ui auto
+git config --global push.default simple
+git config --global credential.helper 'cache --timeout=1800'
+
+if [ ! -d ~/src/ubuntu-init ]; then
+	mkdir -p ~/src
+	git clone https://github.com/d/ubuntu-init ~/src/ubuntu-init
+else
+	pushd ~/src/ubuntu-init
+	git pull --ff-only
+fi
 
 pushd ~/src/ubuntu-init/dotfiles
 make install
